721f77d23f4a5f618da346018c85727e57d404cc
Dave Barner <dbarner@gmail.com>
2016-11-09 22:17:02 -0500

    update for blackjack

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-var score = 15;
-var score_end = 2;
+var bottomCTA = document.getElementbyId('bottom-CTA')
 
-var name = prompt("What's your name?");
-var name=name + "hello!";
-
-alert(name);
-
-console.log("Begin Program");
-
-function home_alert() {
-            alert('ok');
-        }
-        window.onload = home_alert;
-
- console.log("Program Complete")
-
- alert(score+score_end);
+bottomCTA="New Message"
