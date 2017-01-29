0de0bad63a15e052c3699581445bb20c24a242e3
Dave Barner <dbarner@gmail.com>
2017-01-28 20:35:59 -0500

    final close

--- products/bslc/dist/contactus.js
+++ products/bslc/dist/contactus.js
@@ -22,10 +22,10 @@ function removeClass(el, className) {
   }
 }
 
-var learnMoreOptions = document.querySelectorAll(".learnMoreOption");
-var learnMoreView = document.querySelector(".learnMoreView");
+var contactUsOptions = document.querySelectorAll(".contactUsOption");
+var contactUsView = document.querySelector(".contactUsView");
 
-var learnMoreContent = [
+var contactUsContent = [
 	["<h3>By Phone</h3><p>617-817-1823</p><h3>By Email<p>information@baystatelearning.org</p>"],
 	["<h3>George Popham: Executive Director/Instructor</h3><p>george@baystatelearning.org</p><h3>Avery Kallas: Program Director/Instructor</h3><p>avery@baystatelearning.org</p><h3>Marjorie Kukstis: Operations Director</h3><p>marjorie@baystatelearning.org</p><h3>Jack Kallas: Instructor</h3><p>jack@baystatelearning.org</p><h3>Paul Barner: Makerspace Instructor</h3><p>pkbarner@gmail.com</p>"],
 	["<h3></p>"],
@@ -36,20 +36,20 @@ var learnMoreContent = [
 ];
 
 
-for(i=0;i<learnMoreOptions.length;i++) {
+for(i=0;i<contactUsOptions.length;i++) {
 	(function () {
 		var j=i;
-		var optionSelected =learnMoreOptions[i];
+		var optionSelected =contactUsOptions[i];
 		optionSelected.addEventListener("click", function() {
 			console.log("clicked" + (j+1) + "!");
-			learnMoreView.innerHTML = learnMoreContent[j];
+			contactUsView.innerHTML = contactUsContent[j];
 
 			if(hasClass(this,"selected")) {
 				console.log("already selected");
 			} else {
 				var x;
-				for(x=0;x<learnMoreOptions.length;x++) {
-					removeClass(learnMoreOptions[x],"selected");
+				for(x=0;x<contactUsOptions.length;x++) {
+					removeClass(contactUsOptions[x],"selected");
 				};
 				addClass(this,"selected");
 
